1156d3a897eb82c95ee2cf156f2d6c3645c97c19
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-03-24 15:42:09 +0000

    New translations driving.py (Dutch)

--- nl-NL/resources/driving.py
+++ nl-NL/resources/driving.py
@@ -1,14 +1,14 @@
 #!/bin/python3
 
-# Import library code
+# Bibliotheekcode importeren
 from p5 import *
 from random import randint, seed
 
 level = 1
 score = 0
 
-# The draw_obstacle function goes here
-def draw_obstacles():
+# De teken_obstakel functie komt hier
+def teken_obstakels():
   
   global level
   
@@ -16,71 +16,71 @@ def draw_obstacles():
   
   if frame_count % width == width - 1 and level < 10:
     level += 1
-    print('You reached level', level)
+    print('Je hebt level', level, 'bereikt')
     
   for i in range(6 + level):
-    ob_x = randint(0, width) - (frame_count * level)
-    ob_y = randint(0, height) 
-    ob_x %= width # wrap around
-    text('💩', ob_x, ob_y)
+    obstakel_x = randint(0, width) - (frame_count * level)
+    obstakel_y = randint(0, height) 
+    obstakel_x %= width # omwikkelen
+    text('💩', obstakel_x, obstakel_y)
     
-# The draw_player function goes here
-def draw_player():
+# De teken_speler functie komt hier
+def teken_speler():
   
   global score, level
   
-  player_x = int(width * 0.2)
-  player_y = mouse_y
+  speler_x = int(width * 0.2)
+  speler_y = muis_y
   
-  collide = get(player_x + 50, player_y + 15)
-  collide2 = get(player_x + 50, player_y - 15)
-  collide3 = get(player_x, player_y + 15)
-  collide4 = get(player_x, player_y - 15)
-  collide5 = get(player_x - 50, player_y + 15)
-  collide6 = get(player_x - 50, player_y - 15)
+  botsen = get(speler_x + 50, speler_y + 15)
+  botsen2 = get(speler_x + 50, speler_y - 15)
+  botsen3 = get(speler_x, speler_y + 15)
+  botsen4 = get(speler_x, speler_y - 15)
+  botsen5 = get(speler_x - 50, speler_y + 15)
+  botsen6 = get(speler_x - 50, speler_y - 15)
   
-  if player_y > height - 18: # Off the bottom of the screen
-    collide = safe
-    collide3 = safe
-    collide5 = safe
+  if speler_y > height - 18: # Voorbij de onderkant van het scherm
+    botsen = veilig
+    botsen3 = veilig
+    botsen5 = veilig
     
-  elif player_y < 18: # Off the top of the screen
-    collide2 = safe
-    collide4 = safe
-    collide6 = safe
+  elif speler_y < 18: # Voorbij de bovenkant van het scherm
+    botsen2 = veilig
+    botsen4 = veilig
+    botsen6 = veilig
     
-  if collide == safe and collide2 == safe and collide3 == safe and collide4 == safe:
-    image(car, player_x, player_y, 100, 31)
+  if botsen == veilig and botsen2 == veilig and botsen3 == veilig and botsen4 == veilig:
+    image(auto, speler_x, speler_y, 100, 31)
     score += level
   else:
-    text('💥', player_x, player_y)
+    text('💥', speler_x, speler_y)
     level = 0
     
   
 def setup():
-  # Setup your animation here
-  global car
+  # Stel hier je animatie in
+  global auto
   
   size(400, 400)
-  car = load_image('car.png')
+  auto = load_image('car.png')
   image_mode(CENTER)
   
   
 def draw():
-  # Things to do in every frame
-  global score, safe, level
-  safe = color(128)
+  # Dingen om te doen in elk frame
+  global score, veilig, level
+  veilig = color(128)
   
   if level > 0:
-    background(safe)
+    background(veilig)
     fill(255)
     text_size(16)
     text_align(RIGHT, TOP)
     text('Score', width * 0.45, 10, width * 0.5, 20)
     text(str(score), width * 0.45, 25, width * 0.5, 20)
     text_size(20)
-    text_align(CENTER, TOP) # position around the centre, top
-    draw_obstacles()
-    draw_player()
+    text_align(CENTER, TOP) # positie rond het midden, bovenaan
+    teken_obstakels()
+    teken_speler()
   
 run()
