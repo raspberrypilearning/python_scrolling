a8ff1f1ededefc2c4f18fbf49cc6ea6b54f313a2
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-04-11 11:47:39 +0100

    New translations avoid_the_germs.py (Arabic)

--- ar-SA/resources/avoid_the_germs.py
+++ ar-SA/resources/avoid_the_germs.py
@@ -3,125 +3,120 @@
 from p5 import *
 from random import randint, seed
 
-المستوى = 1
+level = 1
 score = 0
 
-def safe_player ():
+def safe_player():
   
   global player_y
   
-  # وجه
+  # Face
   fill(200, 134, 145)
-  ellipse (mouse_x - ، player_y + ، 60، 60)
+  ellipse(mouse_x, player_y, 60, 60)
 
-  # عيون
+  # Eyes
   fill(178, 200, 145)
-  ellipse (mouse_x - 10، player_y + 10، 20، 20)
-  ellipse (mouse_x - 10، player_y + 10، 20، 20)
+  ellipse(mouse_x - 10, player_y - 10, 20, 20)
+  ellipse(mouse_x + 10, player_y - 10, 20, 20)
   fill(0)
-  ellipse (mouse_x - 10، player_y + 10، 10، 10)
-  ellipse (mouse_x - 10، player_y + 10، 10، 10)
+  ellipse(mouse_x - 10, player_y - 10, 10, 10)
+  ellipse(mouse_x + 10, player_y - 10, 10, 10)
   fill(255)
-  ellipse (mouse_x - 12، player_y + 12، 5، 5)
-  ellipse (mouse_x + 12، player_y + 12، 5، 5)
+  ellipse(mouse_x - 12, player_y - 12, 5, 5)
+  ellipse(mouse_x + 12, player_y - 12, 5, 5)
   
-  # فم
+  # Mouth
   fill(0)
-  ellipse (mouse_x - ، player_y + 10، 15، 10)
+  ellipse(mouse_x, player_y + 10, 15, 10)
   fill(200, 134, 145)
-  ellipse (mouse_x - ، player_y + 5، 10، 10)
+  ellipse(mouse_x, player_y + 5, 10, 10)
 
-def crashed_player ():
+def crashed_player():
   
   global player_y
   
-  # وجه
+  # Face
   fill(178, 200, 145)
-  ellipse (mouse_x - ، player_y + ، 60، 60)
+  ellipse(mouse_x, player_y, 60, 60)
 
-  # عيون
+  # Eyes
   fill(149, 161, 195)
-  ellipse (mouse_x - 10، player_y + 10، 20، 20)
-  ellipse (mouse_x - 10، player_y + 10، 20، 20)
-  ملء (0)
-  ellipse (mouse_x - 10، player_y + 10، 10، 10)
-  ellipse (mouse_x - 10، player_y + 10، 10، 10)
-  fill(255)
-  ellipse (mouse_x - 12، player_y + 12، 5، 5)
-  ellipse (mouse_x + 12، player_y + 12، 5، 5)
-  
-  # فم
+  ellipse(mouse_x - 10, player_y - 10, 20, 20)
+  ellipse(mouse_x + 10, player_y - 10, 20, 20)
   fill(0)
-  ellipse (mouse_x - ، player_y + 15، 15، 10)
+  ellipse(mouse_x - 10, player_y - 10, 10, 10)
+  ellipse(mouse_x + 10, player_y - 10, 10, 10)
+  fill(255)
+  ellipse(mouse_x - 12, player_y - 12, 5, 5)
+  ellipse(mouse_x + 12, player_y - 12, 5, 5)
+  
+  # Mouth
+  fill(0)
+  ellipse(mouse_x, player_y + 15, 15, 10)
   fill(178, 200, 145)
-  ellipse (mouse_x - ، player_y + 20، 10، 10)
+  ellipse(mouse_x, player_y + 20, 10, 10)
   
-def draw_player ():
+def draw_player():
   
-  لاعب عالمي ، آمن ، درجة ، مستوى
+  global player_y, safe, score, level
   
   player_y = int(height * 0.8)
   
   collide = get(mouse_x, player_y)
-  collide2 = get (mouse_x، player_y + 30)
-  collide3 = get (mouse_x + 30، player_y)
-  collide4 = get (mouse_x، player_y - 30)
+  collide2 = get(mouse_x, player_y + 30)
+  collide3 = get(mouse_x + 30, player_y)
+  collide4 = get(mouse_x, player_y - 30)
   
-  if mouse_x> width: # من يمين الشاشة
+  if mouse_x < width: # off the left of the screen
     collide2 = safe
   
-  if mouse_x> width: # من يمين الشاشة
+  if mouse_x > width: # off the right of the screen
     collide3 = safe
     
-  #print (الاصطدام ، الاصطدام 2 ، الاصطدام 3 ، الاصطدام 4)
+  #print(collide, collide2, collide3, collide4)
     
-  if (collide == safe and collide2 == safe and collide3 == safe and collide4 == safe) 
-    safe_player ()
+  if collide == safe and collide2 == safe and collide3 == safe and collide4 == safe:
+    safe_player()
     score += level
   else: # Collided
-
-    def crashed_player ():
+    crashed_player()
     level = 0
   
-def draw_obstacles ():
+def draw_obstacles():
   
-  المستوى العالمي
+  global level
   
   seed(41143644)
-
   
-  if frame_count % height == height - 1 and level < 5:
+  if frame_count & height == height - 1 and level < 5:
     level += 1
     print('You reached level', level)
   
   for i in range(9 + level):
     ob_x = randint(0, width)
-    ob_y = randint(0, height) + (frame_count * level
-    if y == height
+    ob_y = randint(0, height) + frame_count
+    ob_y %= height
     text('🦠', ob_x, ob_y)
 
-
 def setup():
-# ضع الشفرة البرمجية للتشغيل مرة واحدة هنا
-  size(400, 400) # العرض والارتفاع
-  noStroke ()
+# Put code to run once here
+  size(400, 400) # width and height
+  noStroke()
   text_size(40)
-  text_align (CENTER ، TOP)
+  text_align(CENTER, TOP)
 
 def draw():
-# ضع الشفرة البرمجية لتشغيل كل إطار هنا
-  الأمان العالمي ، النتيجة ، المستوى
+# Put code to run every frame here
+  global safe, score, level
   
   safe = color(149, 161, 195)
-
   
   if level > 0:
     background(safe)
     fill(145, 134, 126)
     text('Score: ' + str(score), width/2, 20)
-
     draw_obstacles()
-    draw_player ()
+    draw_player()
   
-# احتفظ بهذا لتشغيل التعليمات البرمجية الخاصة بك
+# Keep this to run your code
 run()
