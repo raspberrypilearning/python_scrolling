8b09a838cb80ed9a9b4240abe6637017644dd6c0
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-22 16:06:21 +0000

    New translations main.py (Polish)

--- pl-PL/code/dont_collide_skiing_cat_example/main.py
+++ pl-PL/code/dont_collide_skiing_cat_example/main.py
@@ -1,71 +1,71 @@
 #!/bin/python3
 
-# Import library code
+# Importuj kod biblioteki
 from p5 import *
 from random import randint, seed
 
-speed = 1
-score = 0
+prędkość = 1
+wynik = 0
 
-# The draw_background function goes here
-def draw_obstacles():
-    global speed
+# Funkcja draw_background pojawia się tutaj
+def rysowanie_przeszkody():
+    globalna prędkość
     
     seed(12345678)
     
     if frame_count % height == height - 1 and speed < 5:
-        speed += 1
-        print('You reached level', speed)
+        prędkość += 1
+        Print('osiągnąłeś poziom', prędkość)
       
     for i in range(6):
-        ob_x = randint(0, height)
-        ob_y = randint(0, height) + (frame_count * speed)
-        ob_y %= height  # wrap around
+        ob_x = randint(0, wysokość)
+        ob_y = randint(0, wysokość) + (liczba_klatek * prędkość)
+        ob_y %= wysokość # owijanie
         no_stroke()
         fill(0,255,0)
-        triangle(ob_x + 20, ob_y + 20, ob_x + 10, ob_y + 40, ob_x + 30, ob_y + 40)
-        triangle(ob_x + 20, ob_y + 30, ob_x + 5, ob_y + 55, ob_x + 35, ob_y + 55)
-        triangle(ob_x + 20, ob_y + 40, ob_x + 0, ob_y + 70, ob_x + 40, ob_y + 70)
+        trójkąt(ob_x + 20, ob_y + 20, ob_x + 10, ob_y + 40, ob_x + 30, położnictwo_y + 40)
+        trójkąt(ob_x + 20, ob_y + 30, ob_x + 5, ob_y + 55, ob_x + 35, położnictwo_y + 55)
+        trójkąt(ob_x + 20, ob_y + 40, ob_x + 0, ob_y + 70, ob_x + 40, położnictwo_y + 70)
         fill(150,100,100)
         rect(ob_x + 15, ob_y + 70, 10, 10)
     
-# The draw_player function goes here
+# Funkcja draw_player pojawia się tutaj
 def draw_player():
-    global score, speed, skiing, crashed
+    globalny wynik, prędkość, jazda na nartach, rozbił się
     
-    player_y = int(height * 0.8)
+    player_y = int(wysokość * 0.8)
     
-    fill(safe)
+    fill(bezpieczne)
   
     collide = get(mouse_x, player_y).hex
     
     if collide == safe.hex:
-        image(skiing, mouse_x, player_y, 30, 30)
-        score += speed
+        image(narty, mouse_x, player_y, 30, 30)
+        wynik += prędkość
     else:
-        image(crashed, mouse_x, player_y, 30, 30)
-        speed = 0
+        obraz(rozbity, mysz_x, gracz_y, 30, 30)
+        prędkość = 0
     
   
 def setup(): 
-    # Setup your animation here
+    # Ustaw swoją animację tutaj
     size(400, 400)
     text_size(40)
-    text_align(CENTER, TOP)  # position around the centre
-    global skiing, crashed
-    skiing = load_image('skiing.png')
-    crashed = load_image('fallenover.png')
+    Text_align(ŚRODEK, GÓRA) # pozycja wokół środka
+    globalne narty, rozbił się
+    narciarstwo = load_image('skiing.png')
+    rozbity = load_image('fallenover.png')
   
 def draw():
-    # Things to do in every frame
-    global score, safe, speed, skiing, crashed
-    safe = Color(255)
+    # Rzeczy do zrobienia w każdej klatce
+    globalny wynik, bezpieczny, prędkość, jazda na nartach, rozbił się
+    Bezpieczny = Kolor(255)
   
-    if speed > 0:
-        background(safe) 
+    if prędkość > 0:
+        tło(bezpieczne) 
         fill(0)
-        text('Score: ' + str(score), width/2, 20)
-        draw_obstacles()
+        Text('Wynik: ' + str(wynik), width/2, 20)
+        rysowanie_przeszkody()
         draw_player()
   
 run()
