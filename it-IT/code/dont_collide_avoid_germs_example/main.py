3131198e66d093937641f5ffaf99d900c9749934
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-23 18:55:50 +0100

    New translations main.py (Italian)

--- it-IT/code/dont_collide_avoid_germs_example/main.py
+++ it-IT/code/dont_collide_avoid_germs_example/main.py
@@ -42,78 +42,78 @@ def giocatore_eliminato():
     ellipse(mouse_x - 10, giocatore_y - 10, 20, 20)
     ellipse(mouse_x + 10, giocatore_y - 10, 20, 20)
     fill(0)
-    ellipse(mouse_x - 10, player_y - 10, 10, 10)
-    ellipse(mouse_x + 10, player_y - 10, 10, 10)
+    ellipse(mouse_x - 10, giocatore_y - 10, 10, 10)
+    ellipse(mouse_x + 10, giocatore_y - 10, 10, 10)
     fill(255)
-    ellipse(mouse_x - 12, player_y - 12, 5, 5)
-    ellipse(mouse_x + 12, player_y - 12, 5, 5)
+    ellipse(mouse_x - 12, giocatore_y - 12, 5, 5)
+    ellipse(mouse_x + 12, giocatore_y - 12, 5, 5)
     
-    # Mouth
+    # Bocca
     fill(0)
-    ellipse(mouse_x, player_y + 15, 15, 10)
+    ellipse(mouse_x, giocatore_y + 15, 15, 10)
     fill(178, 200, 145)
-    ellipse(mouse_x, player_y + 20, 10, 10)
+    ellipse(mouse_x, giocatore_y + 20, 10, 10)
   
-def draw_player():
+def disegna_giocatore():
   
-    global player_y, safe, score, level
+    global giocatore_y, safe, punteggio, livello
     
-    player_y = int(height * 0.8)
+    giocatore_y = int(height * 0.8)
     
-    collide = get(mouse_x, player_y).hex
-    collide2 = get(mouse_x, player_y + 30).hex
-    collide3 = get(mouse_x + 30, player_y).hex
-    collide4 = get(mouse_x, player_y - 30).hex
+    collisione = get(mouse_x, giocatore_y).hex
+    collisione2 = get(mouse_x, giocatore_y + 30).hex
+    collisione3 = get(mouse_x + 30, giocatore_y).hex
+    collisione4 = get(mouse_x, giocatore_y - 30).hex
     
-    if mouse_x < width:  # off the left of the screen
-        collide2 = safe.hex
+    if mouse_x < width: # fuori a sinistra dello schermo
+        collisione2 = safe.hex
     
-    if mouse_x > width:  # off the right of the screen
-        collide3 = safe.hex
+    if mouse_x > width:  # fuori a destra dello schermo
+        collisione3 = safe.hex
       
-    #print(collide, collide2, collide3, collide4)
+    #print(collisione, collisione2, collisione3, collisione4)
       
-    if collide == safe.hex and collide2 == safe.hex and collide3 == safe.hex and collide4 == safe.hex:
-        safe_player()
-        score += level
-    else:  # Collided
-        crashed_player()
-        level = 0
+    if collisione == safe.hex and collisione2 == safe.hex and collisione3 == safe.hex and collisione4 == safe.hex:
+        giocatore_salvo()
+        punteggio += livello
+    else: #Collisione
+        giocatore_eliminato()
+        livello = 0
   
-def draw_obstacles():
-    global level
+def disegnare_ostacoli():
+    global livello
     
     seed(41143644)
     
-    if frame_count & height == height - 1 and level < 5:
-        level += 1
-        print('You reached level', level)
+    if frame_count & height == height - 1 and livello < 5:
+        livello += 1
+        print('Hai raggiunto il livello', livello)
     
-    for i in range(9 + level):
-        ob_x = randint(0, width)
-        ob_y = randint(0, height) + frame_count
-        ob_y %= height
-        text('🦠', ob_x, ob_y)
+    for i in range(9 + livello):
+        
+        
+        
+        
 
 def setup():
-    # Put code to run once here
-    size(400, 400)  # width and height
+    # Metti qui sotto il codice che verrà eseguito una sola volta
+    size(400, 400)  # larghezza e altezza
     no_stroke()
     text_size(40)
     text_align(CENTER, TOP)
 
 def draw():
-    # Put code to run every frame here
-    global safe, score, level
+    # Metti qui sotto il codice che verrà eseguito ad ogni cambio di frame
+    global safe, punteggio, livello
   
-    safe = Color(149, 161, 195)
+    
   
-    if level > 0:
-        background(safe)
+    if livello > 0:
+        
         fill(145, 134, 126)
-        text('Score: ' + str(score), width/2, 20)
-        draw_obstacles()
-        draw_player()
+        text('Punteggio: ' + str(punteggio), width/2, 20)
+        disegnare_ostacoli()
+        disegna_giocatore()
   
-# Keep this to run your code
+# Conserva questa parte per eseguire il codice
 run()
