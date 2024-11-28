b0326ade744c8108f95055654580ccc33a271674
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-11-28 12:47:24 +0000

    Update starter

--- en/code/dont-collide-starter/main.py
+++ en/code/dont-collide-starter/main.py
@@ -1,17 +1,25 @@
-#!/bin/python3
-
 from p5 import *
 from random import randint, seed
 
 # Include global variables here
+screen_size = 400
+
+# Draw player function goes here
+
+
+
+# Draw obstacles function goes here
 
 
 def setup():
     # Put code to run once here
+    size(screen_size, screen_size)
+    text_size(40)
 
 
 def draw():
     # Put code to run every frame here
+    
 
   
 # Keep this to run your code
